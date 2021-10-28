e279929335641a354e54c4245049e2714b0cb1c7
Djeyco <48934844+Djeyco@users.noreply.github.com>
2021-10-28 05:51:29 -0400

    SoloDungeons v49

--- Maps/RecursosMap/gd/Desert.gd
+++ Maps/RecursosMap/gd/Desert.gd
@@ -1,29 +1,28 @@
 extends Node2D
 
-var enemy_1 = preload("res://Enemies/Spider.tscn")
-#var objeto = preload("res://Maps/RecursosMap/tscn/Tree.tscn")
+var enemy_1 = preload("res://Enemies/Serpiente.tscn")
+var enemy_2 = preload("res://Enemies/Scorpion.tscn")
 
 func _ready():
+	Global.update_puntos()
 	Global.node_creation_parent = self
 	randomize()
-#	_crear_objeto()
-#func _crear_objeto():
-#	var _cont = 0
-#	for cont in 10:
-#		var randix = rand_range(-192, 384)
-#		var  randiy = rand_range(-256, 576)
-#		var crear = objeto.instance()
-#		add_child(crear)
-#		crear.position.x = randix
-#		crear.position.y = randiy
 
 func _exit_tree():
 	Global.node_creation_parent = null
 
 func _on_Enemy_spawn_timer_timeout():
 	var enemy_position = Vector2(rand_range(-192, 384), rand_range(-256, 576))
-	
 	while enemy_position.x < 640 and enemy_position.x > -80 or enemy_position.y < 360 and enemy_position.y > -45:
 		enemy_position = Vector2(rand_range(-160, 670), rand_range(-90, 390))
-	
 	Global.instance_node(enemy_1, enemy_position, self)
+
+func _on_Enemy_spawn_timer2_timeout():
+	var enemy_position = Vector2(rand_range(-192, 384), rand_range(-256, 576))
+	while enemy_position.x < 640 and enemy_position.x > -80 or enemy_position.y < 360 and enemy_position.y > -45:
+		enemy_position = Vector2(rand_range(-160, 670), rand_range(-90, 390))
+	Global.instance_node(enemy_2, enemy_position, self)
+
+func _on_Timer_timeout():
+	Global.tiempo += 1
+	Global.update_time()
--- Maps/RecursosMap/gd/Global.gd
+++ Maps/RecursosMap/gd/Global.gd
@@ -1,8 +1,21 @@
 extends Node
 
+#variables para el sistema de puntuacion
+var puntos = 0
+var tiempo = 0
+#variables de funciones del mapa y jugador
 var door_name = null
 var node_creation_parent = null
 var player = null
+
+func enemy_killed():
+	puntos += 1
+
+func update_puntos():
+	get_tree().get_nodes_in_group("puntos")[0].text = String(puntos)
+
+func update_time():
+	get_tree().get_nodes_in_group("tiempo")[0].text = String(tiempo)
 
 func instance_node(node, location, parent):
 	var node_instance = node.instance()
