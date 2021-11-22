222b5165dafaceb7fa72757784c07bee28b1b08e
Djeyco <jeyco0009@gmail.com>
2021-11-22 15:37:11 -0400

    SoloDungeon v56

--- ButtonPause/Pausa_DyF.gd
+++ ButtonPause/Pausa_DyF.gd
@@ -10,20 +10,13 @@ func _process(_delta):
 		else :
 			$FondoMenu.show()
 			get_tree().paused = true
-			Global.update_Ptotal()
 
 func _on_Salir_pressed():
 	get_tree().paused = false
 	# warning-ignore:return_value_discarded
 	get_tree().change_scene(targuet_scene)
-	
+
 func _on_Continuar_pressed():
 	if get_tree().paused == true:
 		$FondoMenu.hide()
 		get_tree().paused = false
- 
-func _on_Guardar_pressed():
-	Global.guardar_partida(1)
-
-func _on_Cargar_pressed():
-	Global.cargar_partida(1)
