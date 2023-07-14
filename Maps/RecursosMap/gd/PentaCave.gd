94fd084ee33eede93db89ec8c5a8e2e393c669b8
Djeyco <48934844+Djeyco@users.noreply.github.com>
2023-07-14 17:39:26 -0400

    Vercion final

--- Maps/RecursosMap/gd/PentaCave.gd
+++ Maps/RecursosMap/gd/PentaCave.gd
@@ -6,7 +6,7 @@ export(String, FILE, "*.tscn") var escena
 
 func _on_PentaDesert_body_entered(body):
 	if body.name == "Player":
-		if PlayerStats.max_health >= 6:
+		if PlayerStats.max_health >= 5:
 			MusicaFondo.musica_fondo_off(musica_fondo)
 			# warning-ignore:return_value_discarded
 			get_tree().change_scene("res://Maps/"+escena+".tscn")
